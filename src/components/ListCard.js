bdf6dc1d2b7a64fe043758d45698d1c6fd83fad8
sori kim <65494214+milmilkim@users.noreply.github.com>
2021-09-21 19:04:13 +0900

    카드 수정
    (╯‵□′)╯︵┻━┻

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,42 +1,94 @@
-import { Card, Avatar, Col } from 'antd';
-import { TeamOutlined } from '@ant-design/icons';
+import { Col } from 'antd';
 import { Link } from 'react-router-dom';
+import 'components/ListCard.css';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { Row, Spin } from 'antd';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Card } from 'antd';
 
-const ListCard = ({ children }) => {
-  const {
-    description,
-    thumbnail,
-    profileImg,
-    title,
-    count,
-    city,
-    hashtag,
-    id,
-  } = children;
+const ListCard = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState('');
+  const [item, setItem] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+
   const { Meta } = Card;
 
-  const thumbCrob = {
-    width: '100%',
-    height: '200px',
-    objectFit: 'cover',
-    borderRadius: '10px',
+  const getData = async () => {
+    try {
+      setLoading(true);
+      await axios.get('dummy/dummyJson.json').then(res => {
+        const sortedRes = res.data.sort((a, b) => b.id - a.id);
+        setData(sortedRes.slice(0, 9));
+        setItem(sortedRes.slice(9));
+      });
+    } catch (e) {
+      console.log('-_-+');
+    }
+
+    setLoading(false);
   };
 
+  const moreData = () => {
+    setLoading(true);
+    setData(data.concat(item.slice(0, 9)));
+    setItem(item.slice(9));
+    if (item < 1) {
+      setHasMore(false);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <Col xs={24} sm={12} md={8}>
-      <Link to="/contents">
-        <Card
-          bordered={false}
-          cover={<img style={thumbCrob} alt={description} src={thumbnail} />}
-        >
-          <Meta avatar={<Avatar src={profileImg} />} title={title} />
-          {city} <TeamOutlined style={{ fontSize: 14 }} />
-          {count}
-          <br />#{hashtag} <br />
-          {id}
-        </Card>
-      </Link>
-    </Col>
+    <>
+      {loading ? (
+        <div className="card__spin">
+          <Spin tip="Loading..." />
+        </div>
+      ) : (
+        <div>
+          <InfiniteScroll
+            dataLength="10"
+            next={moreData}
+            loader={
+              <div className="card__spin">
+                <Spin tip="Loading..." />
+              </div>
+            }
+            hasMore={hasMore}
+            endMessage={
+              <p style={{ textAlign: 'center' }}>
+                <b>Yay! You have seen it all</b>
+              </p>
+            }
+          >
+            <Row gutter>
+              {data.map(list => (
+                <Col xs={24} sm={8} md={8}>
+                  <Link to="/contents">
+                    <Card
+                      hoverable
+                      style={{ width: '100%' }}
+                      cover={<img alt="example" src={list.thumbnail} />}
+                    >
+                      <Meta
+                        title={list.title}
+                        description={list.description}
+                      />
+                    </Card>
+                  </Link>
+                </Col>
+              ))}
+            </Row>
+          </InfiniteScroll>
+        </div>
+      )}{' '}
+    </>
   );
 };
 

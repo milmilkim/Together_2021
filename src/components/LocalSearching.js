03d4d59ec488c90bb3ebc85000e290a01e9f3a17
milmilkim <65494214+milmilkim@users.noreply.github.com>
2021-09-23 15:50:44 +0900

    (╯‵□′)╯︵┻━┻

--- src/components/LocalSearching.js
+++ src/components/LocalSearching.js
@@ -7,31 +7,34 @@ const LocalSeraching = () => {
   const { Search } = Input;
 
   const REST_API_KEY = 'b848a4ccc1802d07fa250ac646972888';
-  const [query, setQuery] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
   const [addressObject, setAddressObject] = useState([]);
-  const page = '1'; //페이지 
+  const page = '1'; //페이지
 
   const onSearch = value => {
     setQuery(value);
-    console.log(value);
   };
 
   const getData = async () => {
     setLoading(true);
     await axios({
       method: 'get',
-      url: `https://dapi.kakao.com//v2/local/search/keyword.json?page=${page}&size=15&sort=accuracy&query=${query}`,
+      url: 'https://dapi.kakao.com//v2/local/search/keyword',
       headers: { Authorization: 'KakaoAK ' + REST_API_KEY },
+      params: {
+        page: page,
+        size: 10,
+        query: query,
+      },
     }).then(res => {
       setAddressObject(res.data.documents);
-      console.log(addressObject);
     });
     setLoading(false);
   };
 
   useEffect(() => {
-    getData();
+    if (query !== '') getData();
   }, [query]); //값이 변경될 때마다 실행
 
   return (

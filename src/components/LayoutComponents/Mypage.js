8c71db6dd7600a99858dcb913343d19aced81bca
sori kim <65494214+milmilkim@users.noreply.github.com>
2021-10-03 11:55:44 +0900

    Lee ej (#9)
    * 마이페이지 수정1
    
    * 마이페이지 수정1
    
    * 마이페이지 디자인개선
    
    Co-authored-by: ohyeah1217 <ohyeah8716@gmail.com>

--- src/components/LayoutComponents/Mypage.js
+++ src/components/LayoutComponents/Mypage.js
@@ -3,6 +3,7 @@ import 'antd/dist/antd.css';
 import { Form, Button, Input, Card, Col, Row } from 'antd';
 import './Mypage.css';
 import { Link } from 'react-router-dom';
+import Myprofile from './Myprofile';
 
 const Mypage = () => {
   return (
@@ -22,7 +23,7 @@ const Mypage = () => {
       <div className="nicknamewrap">
         <div className="emailbottom">자기소개</div>
         <Col span={22}>
-          <Input
+          <textarea
             className="intro-input"
             type="text"
             placeholder="간단한 자기소개를 적어주세요."
@@ -31,62 +32,59 @@ const Mypage = () => {
         </Col>
       </div>
 
-      <div className="cardwrap">
-        <h2 className="subtitle">선호 컨텐츠</h2>
+      <div className="nicknamewrap">
+        <h2 className="emailbottom">선호 컨텐츠</h2>
         <div className="site-card-wrapper">
           <Row gutter={16}>
             <Col span={8}>
               <Card
                 className="carddetail"
                 title="축구"
-                style={{ width: 300, height: 200 }}
+                style={{ width: '100%', height: 200 }}
               >
-                <img src="" alt="사진" />
+                <img src="" alt="픽토그램" />
               </Card>
             </Col>
             <Col span={8}>
               <Card
                 className="carddetail"
                 title="야구"
-                style={{ width: 300, height: 200 }}
+                style={{ width: '100%', height: 200 }}
               >
-                <img src="" alt="사진" />
+                <img src="" alt="픽토그램" />
               </Card>
             </Col>
             <Col span={8}>
               <Card
                 className="carddetail"
                 title="농구"
-                style={{ width: 300, height: 200 }}
+                style={{ width: '100%', height: 200 }}
               >
-                <img src="" alt="사진" />
+                <img src="" alt="픽토그램" />
               </Card>
             </Col>
           </Row>
         </div>
       </div>
-      <Link to="./Myprofile">
-        <Button
-          style={{ float: 'right' }}
-          type="primary"
-          shape="round"
-          size="large"
-        >
+      <Button
+        style={{ float: 'right', marginBottom: '30px' }}
+        type="primary"
+        shape="round"
+        size="large"
+      >
+        <Link to="/Myprofile" components={Myprofile}>
           확인
-        </Button>
-      </Link>
+        </Link>
+      </Button>
     </>
   );
 };
 
 export default Mypage;
 
-/* 
-1. TopProfile에서 버튼 누르면 Myprofile로 넘어가게하기
-2. Myprofile에서 수정버튼 누르면 Mypage로 넘어가게하기
-3. Mypage에서 수정할 내용 입력 후 확인 버튼 누르면 내용 저장후 Myprofile로 넘어가게하기
-4. Mypage에서 소개글 입력창 개선
-5. 로그인 하지 않을시에 TopProfile에서 내 프로필 버튼 누르면 Login창으로 넘어가기
+/* TODO
+
+1. Mypage.js에서 수정한내용 Myprofile.js에서 반영되게하기
 
 제가 많이 부족해서 죄송합니다...
 */
--- src/components/LayoutComponents/Myprofile.js
+++ src/components/LayoutComponents/Myprofile.js
@@ -3,66 +3,68 @@ import 'antd/dist/antd.css';
 import { Form, Button, Card, Col, Row } from 'antd';
 import './Mypage.css';
 import { Link } from 'react-router-dom';
+import Mypage from './Mypage';
 
 const Myprofile = () => {
   return (
     <>
       <Form className="nicknamewrap">
-        <div className="emailbottom">email</div>
+        <div className="emailbottom">email(예:asdfasdf@gamil.com)</div>
         <Col span={22}>
-          <span style={{ marginLeft: '4%' }}>닉네임</span>
+          <span style={{ marginLeft: '4%' }}>닉네임(예:고양이)</span>
         </Col>
       </Form>
 
       <div className="nicknamewrap">
-        <div className="emailbottom">소개 글</div>
+        <div className="emailbottom">자기소개</div>
         <Col span={22}>
           <span className="emaildetail" />
         </Col>
       </div>
 
-      <div className="cardwrap">
-        <h2 className="subtitle">선호 컨텐츠</h2>
+      <div className="nicknamewrap">
+        <h2 className="emailbottom">선호 컨텐츠</h2>
         <div className="site-card-wrapper">
           <Row gutter={16}>
             <Col span={8}>
               <Card
                 className="carddetail"
                 title="축구"
-                style={{ width: 300, height: 200 }}
+                style={{ width: '100%', height: 200 }}
               >
-                <img src="" alt="사진" />
+                <img src="" alt="픽토그램" />
               </Card>
             </Col>
             <Col span={8}>
               <Card
                 className="carddetail"
                 title="야구"
-                style={{ width: 300, height: 200 }}
+                style={{ width: '100%', height: 200 }}
               >
-                <img src="" alt="사진" />
+                <img src="" alt="픽토그램" />
               </Card>
             </Col>
             <Col span={8}>
               <Card
                 className="carddetail"
                 title="농구"
-                style={{ width: 300, height: 200 }}
+                style={{ width: '100%', height: 200 }}
               >
-                <img src="" alt="사진" />
+                <img src="" alt="픽토그램" />
               </Card>
             </Col>
           </Row>
         </div>
       </div>
       <Button
-        style={{ float: 'right' }}
+        style={{ float: 'right', marginBottom: '30px' }}
         type="primary"
         shape="round"
         size="large"
-        onClick={this.gotoMypage}
       >
-        수정
+        <Link to="/Mypage" components={Mypage}>
+          수정
+        </Link>
       </Button>
     </>
   );
